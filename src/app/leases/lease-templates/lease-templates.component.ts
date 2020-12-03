7933a752952d664bcb4c0341788546d20692969c
Dylan Roberts <dtroberts@Knights.ucf.edu>
2020-12-02 20:58:04 -0500

    Fixed layout issue with the document editor component

--- src/app/leases/lease-templates/lease-templates.component.ts
+++ src/app/leases/lease-templates/lease-templates.component.ts
@@ -4,15 +4,15 @@ import {
 } from '@syncfusion/ej2-angular-documenteditor';
 
 @Component({
-    selector: 'app-lease-templates',
-    template: `<div style="width:100%;height:330px"><button ejs-button (click)="saveAsDocx()" >Save</button>
-    <ejs-documenteditorcontainer #document_editor_container_component id="container" serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/documenteditor/" style="width:100%;height:100%;display:block" [isReadOnly]=false [enableEditor]=true [enableWordExport]=true> </ejs-documenteditorcontainer></div>`,
+    selector: 'app-container',
+    template: `<button ejs-button (click)="saveAsDocx()" >Save</button>
+    <ejs-documenteditorcontainer #document_editor  id="container" serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/documenteditor/" [isReadOnly]=false [enableEditor]=true [enableWordExport]=true> </ejs-documenteditorcontainer>`,
     encapsulation: ViewEncapsulation.None,
     providers: [ToolbarService, EditorService, SelectionService, SfdtExportService,WordExportService]
 })
 
 export class LeaseTemplatesComponent {
- @ViewChild('document_editor_container_component')
+ @ViewChild('document_editor')
  public documentEditorContainerComponent: DocumentEditorContainerComponent;
  
  public saveAsDocx() :void {
