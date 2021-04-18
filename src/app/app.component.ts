5e40f5306ba46816d6608055eba64c83e23d93d9
Dylan Roberts <dtroberts@Knights.ucf.edu>
2021-04-18 13:04:27 -0400

    Fixed up initial login button hovering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {AccountService} from './services/account.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html',  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit{
   constructor(private router: Router,
